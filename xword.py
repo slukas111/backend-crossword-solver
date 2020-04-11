f3543e8725096e49eca116b03bf1a1f3e20bd3c9
slukas111 <sashamati@gmail.com>
2020-04-10 18:59:49 -0700

    pseudo code going to read dictionary

--- xword.py
+++ xword.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Sasha Lukas"
 
 # YOUR HELPER FUNCTION GOES HERE
 
 
 def main():
-    with open('dictionary.txt') as f:
+    with open('dictionary.txt', 'r') as f:
         words = f.read().split()
+        f.close()
+        return words
+        print(words)
 
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
