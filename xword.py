0e9b079a0a2b57f69d51d19946adef5925b21154
slukas111 <sashamati@gmail.com>
2020-04-10 19:12:32 -0700

    still errors

--- xword.py
+++ xword.py
@@ -5,6 +5,16 @@
 __author__ = "Sasha Lukas"
 
 # YOUR HELPER FUNCTION GOES HERE
+def helper_function(test_word, dict):
+    word_check = len(test_word)-test_word.count(' ')
+    for word in dict:
+        letter = 0
+        match = 0
+        if len(word) == len(test_word):
+            for letter in test_word:
+                match += 1
+            else:
+                letter += 1
 
 
 def main():
@@ -12,9 +22,9 @@ def main():
         words = f.read().split()
         f.close()
         return words
-        print(words)
 
-    test_word = raw_input(
+
+    test_word = raw_input (
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
 
     # YOUR ADDITIONAL CODE HERE
